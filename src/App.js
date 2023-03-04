a728436b4c8f5503258a955d2cb9359b36ba65a2
Nyx-95 <shaikhshady6@gmail.com>
2023-03-04 05:27:44 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect, createContext } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import './style.css';
 import { Layout } from './Main/layout';
 import { Sidebar } from './Main/Component/sidebar';
 
+export const SidebarContext = createContext({ sideBar: false, setSideBar: () => { } })
+
 function App() {
   const [sideBar, setsideBar] = useState(false);
   const getSideBarPos = (value) => {
     setsideBar(!value);
-    // console.log(sideBar, "sideBar")
   }
+  
   return (
     <>
-
-      <Sidebar sideBarWidth={sideBar} />
-      <div className={sideBar ? 'AppFull' : 'App'}>
-        {/* <Routes>
+      <SidebarContext.Provider value={{ sideBar: sideBar, setSideBar: setsideBar }}>
+        <Sidebar  />
+        <div className={sideBar ? 'AppFull' : 'App'}>
+          {/* <Routes>
             <Route element={Sidebar}/>
         </Routes> */}
 
-        <Layout getAsideValue={getSideBarPos}></Layout>
-      </div>
+          <Layout></Layout>
+        </div>
+      </SidebarContext.Provider>
+
     </>
   );
 }
